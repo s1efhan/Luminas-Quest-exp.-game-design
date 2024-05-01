9c9a7a60cdd5fa81b5993c8eea5ab642549232ba
zy4784s <zakiyah.yon@alumni.fh-aachen.de>
2024-05-01 12:55:13 +0200

    added door animation
    added door animation

---
+++ Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool openTrigger = false;
+    //[SerializeField] private float doorOpenDelay = 5f; // Delay time before the door opens again
+    //[SerializeField] private float doorCloseDelay = 5f; // Delay time before the door automatically closes
+    private bool doorOpen = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")&& doorOpen == false)
+        {
+            if (openTrigger)
+            {
+                myDoor.Play("DoorOpen", 0, 0.0f);
+                doorOpen = true;
+                //StartCoroutine(CloseDoorAfterDelay());
+            }
+        }
+    }
+
+    /*IEnumerator CloseDoorAfterDelay()
+    {
+        yield return new WaitForSeconds(doorCloseDelay);
+        myDoor.Play("DoorClose", 0, 0.0f);
+    }*/
+}
+
