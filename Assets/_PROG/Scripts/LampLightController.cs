2bbc36e92f35867d702238ffe228a5d158973c62
zy4784s <zakiyah.yon@alumni.fh-aachen.de>
2024-06-02 23:31:51 +0200

    lampe animation update
    lampe animation update

--- Assets/_PROG/Scripts/LampLightController.cs
+++ Assets/_PROG/Scripts/LampLightController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LampLightController : MonoBehaviour
 {
     public Light lampLight; // Reference to the Light component
-    public float fadeDuration = 5.0f; // Duration over which the light will fade out
+    public ParticleSystem flameParticleSystem; // Reference to the ParticleSystem component
+    public float fadeDuration = 5.0f; // Duration over which the light and particle system will fade out
     private float initialIntensity;
+    private ParticleSystem.EmissionModule emissionModule;
+    private ParticleSystem.MainModule mainModule;
+    private float initialEmissionRate;
+    private float initialStartSize;
 
     void Start()
     {
@@ -14,13 +18,25 @@ public class LampLightController : MonoBehaviour
         {
             lampLight = GetComponent<Light>();
         }
+
+        if (flameParticleSystem == null)
+        {
+            flameParticleSystem = GetComponent<ParticleSystem>();
+        }
+
         initialIntensity = lampLight.intensity;
+        emissionModule = flameParticleSystem.emission;
+        mainModule = flameParticleSystem.main;
+        initialEmissionRate = emissionModule.rateOverTime.constant;
+        initialStartSize = mainModule.startSize.constant;
+
         StartFading();
     }
 
     public void StartFading()
     {
         StartCoroutine(FadeOutLight());
+        StartCoroutine(FadeOutFlame());
     }
 
     private IEnumerator FadeOutLight()
@@ -35,5 +51,23 @@ public class LampLightController : MonoBehaviour
         }
 
         lampLight.intensity = 0f;
+    }
+
+    private IEnumerator FadeOutFlame()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newEmissionRate = Mathf.Lerp(initialEmissionRate, 0f, elapsedTime / fadeDuration);
+            float newStartSize = Mathf.Lerp(initialStartSize, 0f, elapsedTime / fadeDuration);
+            emissionModule.rateOverTime = newEmissionRate;
+            mainModule.startSize = newStartSize;
+            yield return null;
+        }
+
+        emissionModule.rateOverTime = 0f;
+        mainModule.startSize = 0f;
     }
 }
