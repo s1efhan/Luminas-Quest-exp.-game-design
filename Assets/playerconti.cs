9f58a6c012d362f6f9b35cff3a003ba3309837af
s1efhahn <stefan.theissen@mail.de>
2024-06-07 11:12:07 +0200

    Animationen Fixed
    Paar Transitions hinzugefügt

--- Assets/playerconti.cs
+++ Assets/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79535fab919896ae130870132829f95eb47f103039529b3f3c1acb022703fa02
-size 5040
+oid sha256:8dfce83fc06efdf18a4e232edf59b27efaf686e1ad7ab5625b2e1ce9507bd6e7
+size 5043
