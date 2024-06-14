bb2219600d4ec6a678d4c8943e495c1415406109
Malte <malte.kube@alumni.fh-aachen.de>
2024-06-14 19:47:01 +0200

    Performance + Trees

--- Assets/CombineMeshes.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:566c269d4514e43739272885d0bb94439e5720d68a3c739970aafec502757376
-size 1284
--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76f177cb0557adc67c83694cbfb2ad249762e1c3089941c272cb8aa3437a18cc
-size 6721
+oid sha256:b954a75736f9c95763422d41076dc524bbf113e9818fc8ad1e4c4f82180998af
+size 6716
