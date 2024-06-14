34a50957769164ca2df83343b5b4010edb8ad81c
Malte <malte.kube@alumni.fh-aachen.de>
2024-06-15 01:37:38 +0200

    Playercharactercontroller reform

--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b954a75736f9c95763422d41076dc524bbf113e9818fc8ad1e4c4f82180998af
-size 6716
+oid sha256:1b294009c111cba83cfffda3d2a7c763ef041b32e98daa6ccc542f8120b09f57
+size 9198
