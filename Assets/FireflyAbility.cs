a445e84d03aea16e8052d550349e3cfa7000fe95
Malte <malte.kube@alumni.fh-aachen.de>
2024-06-18 17:44:04 +0200

    Firefly functions
    Auswählen über mausrad, E zum aktivieren. nur in Fireflys möglich

--- Assets/FireflyAbility.cs
+++ Assets/FireflyAbility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a3d35e59182be72b82d036490a51751f57a86ff8170cbb0ea6c4ed596165a05
-size 1952
+oid sha256:6f7373a4d0a807ce3d7f92279112bbcba3dc892a389514de74ce8d35c5ebd1e6
+size 1956
--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:185eaea559bf1a090273ba820762bcd5878adb009a75380e02a46887b69726db
-size 14171
+oid sha256:08521c0ea553553a21572e3b1337a231b31d067b9abd8a6febc418dd4326c51d
+size 15163
