8d1f21132b9ed86da7408f1591a647959a20cdb3
Malte <malte.kube@alumni.fh-aachen.de>
2024-06-24 19:46:58 +0200

    delete

--- Assets/FireflyAbility.cs
+++ Assets/FireflyAbility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f7373a4d0a807ce3d7f92279112bbcba3dc892a389514de74ce8d35c5ebd1e6
-size 1956
+oid sha256:41b09c4fac2d2f92e4b18337b7745d182daf3f22f83c9a012da0219938c2dcf8
+size 2028
---
+++ Assets/_PROG/Scripts/lampbroke.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:812da9ee13f9ed9fba5561e981a75e3f577eca858cba1ad259820280f8cea52c
+size 1073
