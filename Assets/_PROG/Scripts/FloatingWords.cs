0dfd8260303391d0057d4d90645ea96de12426a5
s1efhahn <stefan.theissen@mail.de>
2024-06-27 11:09:39 +0200

    FloatingWords Compile Error fixed

--- Assets/_PROG/Scripts/FloatingWords.cs
+++ Assets/_PROG/Scripts/FloatingWords.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53da04d89883ff418738510eaecd96c296950494fcff8446803fd06a20034c0a
-size 9192
+oid sha256:409ebcb8ffd95e381f7230c8a4ae121c0bdf7e0a891c7d8d2c71908c94d60474
+size 9285
