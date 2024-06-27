843f660d4ea97b94c8ad3c0dc232dfd29a239889
s1efhahn <stefan.theissen@mail.de>
2024-06-27 11:31:01 +0200

    Running & Walking Audio reaktiviert

--- Assets/_PROG/Scripts/CharacterAudio.cs
+++ Assets/_PROG/Scripts/CharacterAudio.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d8cb8f421eedd8bab6b1fd29e6f2135bc1df007e1dfc802fbf038dfc9f9d97c
-size 1628
+oid sha256:c52d645facd78a9d886d30a3d5deeba2dc02e0c18d37dc7f358091e93d3fdd8e
+size 1647
