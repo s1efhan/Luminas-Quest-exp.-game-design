3e5839638f2f61a7246335dc01fa0944acfcc1ab
Shahed Abdalla <118661293+shahed-gif@users.noreply.github.com>
2024-06-30 02:06:03 +0200

    Auf URP umgestellt
    Braucht allerdings mehr Arbeit mit den Bäumen, da der Texture von ihnen nicht kompatibel war.

--- Assets/BrokenVector/LowPolyDungeon/Demo Scenes/PlayerMovement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3ebdcf31146c2ba6aa8708505b85264158c3cde3dde8bccca8bfd8ff8640ebd1
-size 1184
---
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:92b47b6505deb1dc50f9cdaea6e51b1d9ee8b41091ef7e808ae12f62fe28aa71
+size 1463
