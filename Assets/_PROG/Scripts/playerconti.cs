436e224df1cc5eb579e946f03d9d1c7f995e1bac
s1efhahn <stefan.theissen@mail.de>
2024-07-01 14:22:37 +0200

    Switch Ability Bug Fixed
    Switch Ability Bug Fixed - Abilitys brechen jetzt ab, wenn man wechselt mit dem Mausrad

--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:251c1c449ca3cb748ea27849e14b1697676f77eca67135b279396a389378da82
-size 18704
+oid sha256:916dd63071b39e5e04d9ecf6b4a9e625a9b181cd7b710a800e987be9bd2698db
+size 19369
