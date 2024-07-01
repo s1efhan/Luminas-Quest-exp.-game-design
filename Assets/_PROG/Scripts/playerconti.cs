a41a5a267c39282bfb6303608dd176856a4b948c
s1efhahn <stefan.theissen@mail.de>
2024-07-01 11:54:09 +0200

    Ändern der Ability Namen
    Neue Namen: Imitation, Spiegelbild und Anker

--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cf97a0d50df90a421701062ad665047acc3b9b8d28b744b4bba81b2cd5229a0
-size 18372
+oid sha256:251c1c449ca3cb748ea27849e14b1697676f77eca67135b279396a389378da82
+size 18704
