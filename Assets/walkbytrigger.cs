2be7f506445438b63edcb0bff898c6adfbc0299b
Malte <malte.kube@alumni.fh-aachen.de>
2024-07-04 02:06:16 +0200

    pilze leuchten

--- Assets/walkbytrigger.cs
+++ Assets/walkbytrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bcfe6fac5b85a3501c5df1d24252a154f46e60f65507ac6eda589f86f033d55
-size 523
+oid sha256:d4e6cea3439d586ee0fd48ddfe6201c41978edde4f187a4686f6a2baf5c89fe0
+size 816
