18bd05b1ae6c1ebb7c4d3d2e6937d9517a1887cd
Shahed Abdalla <118661293+shahed-gif@users.noreply.github.com>
2024-07-06 13:18:54 +0200

    Zweite Rätsel fertig !!
    Jetzt funktioniert das Rätsel gut.

--- Assets/_PROG/Scripts/playerconti.cs
+++ Assets/_PROG/Scripts/playerconti.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e14a4b01b7f6f7de5cc9f12bdfb07e3960af9093d813772b9c42ddd7e0f63ae8
-size 19652
+oid sha256:52e9077c88357048d63c6931bfcf2258fb7f15cb6d6e4a9982bdc312430d5f2d
+size 19127
