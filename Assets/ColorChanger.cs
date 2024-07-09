666d19168dc2d3f315885660fe33ff77d8612270
hh9854s <hussein.hajeer@alumni.fh-aachen.de>
2024-07-09 23:28:36 +0200

    Canvas Überarbeitung und drittes Rätsel verbessert

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4ae94e3c343c73b5b1393101ff793f48aca1f774cf0d6ae75ff90223bb2f690
+size 1870
---
+++ Assets/TextHoverEffect.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42976000aa9b5020321b86545985c7ce168db8332a84b4906c39d8e1bcd9c45c
+size 611
